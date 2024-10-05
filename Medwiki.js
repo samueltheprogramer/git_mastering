87fcd4924f0b4f2add69f3e9ac07924de89101ca
Samuel <samuel.siamintern@gmail.com>
2024-10-05 19:33:54 +0530

    Completed Medwiki UI

---
+++ Medwiki.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Medwiki = () => {
+  return (
+    <View>
+      <Text>Medwiki</Text>
+    </View>
+  );
+};
+
+export default Medwiki;
